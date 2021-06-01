f860d21e764ec3ce59336eb11238bddac79445b8
Timothy Foster <tf.auroratide@gmail.com>
2021-06-01 09:22:12 -0500

    Initial commit

---
+++ export.bat
@@ -0,0 +1,20 @@
+@echo off
+
+SET PACK_NAME=Marscraft
+SET MC_RESOURCE_PACKS=%appdata%\.minecraft\resourcepacks
+
+IF EXIST "%MC_RESOURCE_PACKS%" (
+    ECHO Exporting %PACK_NAME% to %MC_RESOURCE_PACKS%...
+) ELSE (
+    ECHO ERROR: Could not find Minecraft in its default location! Exiting.
+    EXIT
+)
+
+SET PACK_LOCATION=%MC_RESOURCE_PACKS%\%PACK_NAME%
+
+IF EXIST "%PACK_LOCATION%" (
+    ECHO Found existing %PACK_NAME% pack. Removing first.
+    RMDIR /S /Q "%PACK_LOCATION%"
+)
+
+ROBOCOPY "%cd%" "%PACK_LOCATION%" /E
